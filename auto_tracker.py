fceaa5373475473021877945b77c383519d6d932
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2025-07-22 09:14:41 +0200

    Update auto_tracker.py

--- auto_tracker.py
+++ auto_tracker.py
@@ -90,7 +90,7 @@ st.sidebar.markdown("""
     padding: 15px;
     margin-top: 10px;
     margin-bottom: 20px;
-    background-color: #f0f4f8;
+    background-color: purple;
     border: 1px dashed #007acc;
     border-radius: 6px;
 }
