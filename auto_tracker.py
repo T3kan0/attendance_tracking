3eb298c4328a05b1da3f3fe9557b7b02e98a9d46
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2025-07-22 09:30:06 +0200

    Update auto_tracker.py

--- auto_tracker.py
+++ auto_tracker.py
@@ -90,7 +90,7 @@ st.markdown("""
     padding: 15px;
     margin-top: 10px;
     margin-bottom: 20px;
-    background-color: #CBC3E3;
+    background-color: #a569bd;
     border: 1px dashed #007acc;
     border-radius: 6px;
 }
