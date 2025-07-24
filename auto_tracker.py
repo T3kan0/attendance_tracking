42bd46e3671ca80add3feb9e0f8d224c4df3977e
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2025-07-24 20:47:37 +0200

    Update auto_tracker.py

--- auto_tracker.py
+++ auto_tracker.py
@@ -144,7 +144,7 @@ st.sidebar.markdown("""
 c1, c2 = st.columns(2)
 
 with c1:
-    st.markdown("<h6 style='text-align: center; color: #800080;'> OneDrive Data (df1) </h6>", unsafe_allow_html=True)
+    st.markdown("<h6 style='text-align: center; color: green;'> OneDrive Data (df1) </h6>", unsafe_allow_html=True)
     df1_file = st.file_uploader("", type=["csv"], label_visibility="collapsed")
 
 with c2:
