4e9b308c55a7109ff5ebc7f9a2addc06666ebac2
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2025-07-24 20:45:55 +0200

    Update auto_tracker.py

--- auto_tracker.py
+++ auto_tracker.py
@@ -144,11 +144,11 @@ st.sidebar.markdown("""
 c1, c2 = st.columns(2)
 
 with c1:
-    st.markdown("<h6 style='text-align: center;'> OneDrive Data (df1) </h6>", unsafe_allow_html=True)
+    st.markdown("<h6 style='text-align: center; color: #800080;'> OneDrive Data (df1) </h6>", unsafe_allow_html=True)
     df1_file = st.file_uploader("", type=["csv"], label_visibility="collapsed")
 
 with c2:
-    st.markdown("<h6 style='text-align: center;'> PeopleSoft Data (df2) </h6>", unsafe_allow_html=True)    
+    st.markdown("<h6 style='text-align: center; color: #800080;'> PeopleSoft Data (df2) </h6>", unsafe_allow_html=True)    
     df2_file = st.file_uploader(" ", type=["csv"], label_visibility="collapsed")
 
 
