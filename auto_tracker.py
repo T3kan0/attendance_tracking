72574eea1064c812e8d891a413067253c011f990
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2025-07-27 10:48:55 +0200

    Update auto_tracker.py

--- auto_tracker.py
+++ auto_tracker.py
@@ -13,6 +13,7 @@ import plotly.express as px
 from dateutil import parser
 import re
 from datetime import datetime
+from sqlalchemy import create_engine
 
 st.set_page_config(layout="wide")
 #st.markdown("![Alt Text](https://i.postimg.cc/zX2TGD2X/linegraph.png)")
