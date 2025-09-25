45003f009961814f6e602e9913b7fbe4a17ec289
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2025-09-25 17:56:08 +0200

    Update auto_tracker.py

--- auto_tracker.py
+++ auto_tracker.py
@@ -45,7 +45,7 @@ db = pd.DataFrame(response1.data)
 response2 = (
     supabase2.table("sessions")
     .select("*")
-    .gte("Tutorial Date", "2025-08-01")
+    .gte("Tutorial Date", "2025-08-20")
     .order("Tutorial Date")  # correct format
     .range(0, 4999)
     .execute()
