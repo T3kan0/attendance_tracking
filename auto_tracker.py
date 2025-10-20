e30d8ff8bfe83be3fe690f565a98ba12c5fa1084
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2025-10-20 17:51:39 +0200

    Update auto_tracker.py

--- auto_tracker.py
+++ auto_tracker.py
@@ -36,7 +36,7 @@ supabase1: Client = create_client(SUPABASE_URL1, SUPABASE_KEY1)
 supabase2: Client = create_client(SUPABASE_URL2, SUPABASE_KEY2)
 
 # --- Get full date range from Supabase (only once)
-date_range = supabase2.table("sessions").select("Tutorial Date").execute()
+date_range = supabase2.table("sessions").select("Tutorial Date")
 
 
 # Date inputs for user
